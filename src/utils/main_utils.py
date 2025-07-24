3e4ad57bb18a0c714d62bb35bded128b0fe73624
melwintjoshy <melwin465@gmail.com>
2025-07-24 15:57:45 +0530

    updated

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -12,6 +12,7 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+import stat
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
