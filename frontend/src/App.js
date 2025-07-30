17b717db8ab595e0932cd5eb09d990f3796d947b
Prince <psingh80436@gmail.com>
2025-07-30 23:10:43 +0530

    Wrapped router by authprovider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-// App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GitHubRepoInput from './GitHubRepoInput';
+import LoginPage from './components/LoginPage';
+import { AuthProvider } from './context/AuthContext'; 
 
 function App() {
-  return <GitHubRepoInput />;
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<GitHubRepoInput />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
